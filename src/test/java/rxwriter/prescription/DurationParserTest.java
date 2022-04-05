6fb73d78f0bb332169449e207de9a725bdaa347e
Jim Weaver <weaver.je@gmail.com>
2022-04-04 19:21:24 -0500

    after create unit tests module

---
+++ src/test/java/rxwriter/prescription/DurationParserTest.java
@@ -0,0 +1,14 @@
+package rxwriter.prescription;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DurationParserTest {
+
+    @Test
+    public void parseDurationWithValidUnitAndQuantity() {
+
+    }
+
+}
